1ea5dc46a7371df2ec5d4a76b7fa41cdf0744da9
Emmanuelgithunt <emma2004ife@gmail.com>
2023-09-26 10:37:31 +0100

    just added a new function and a style to the button

--- script.js
+++ script.js
@@ -15,5 +15,10 @@ function Handlesubmit() {
 };
 
 function addTask(text) {
-    console.log(text);
+    const taskItem = document.createElement('li');
+    taskItem.className = 'task-item';
+    taskItem.innerHTML = `${text}
+    <button class="delete-btn">Delete</button>
+    <button class="edit-btn">Edit</button>`;
+    tasklist.appendChild(taskItem)
 }
